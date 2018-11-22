cc955a1a49b9cd4beb280ced96dfd973b14c5bed
lucamorellato <luca.morellato@gmail.com>
2018-11-22 17:45:05 -0500

    Added some preprosstuff working on general functionality.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // GLOBAL STUFF
 const runApp = {};
 runApp.apiKey = `AIzaSyBXaDLbvS9m_4euOEdcVxbnybx8D7202C4`;
+runApp.apiKeyTwo = `12db71466624332600c66c1d7e474f6d`;
 let userlocation;
 let locationReturn = {};
-runApp.apiKeyTwo = `12db71466624332600c66c1d7e474f6d`;
+const blurbArray =  [
+    ["Comment One", "Comment Two", "Comment Three"],
+    ["Comment Four", "Comment Five", "Comment 6ix"],
+    ["Comment Seven", "Comment Eight", "Comment Nine"]
+];
 
 // LOCATION FUNCTION & GOOGLE API AJAX CALL
 runApp.geocode = function (userlocation) {
@@ -15,7 +20,7 @@ runApp.geocode = function (userlocation) {
             address: userlocation
         }
     }).then((res) => {
-        // console.log(res);
+        console.log(res);
         // let geoLocation = res.results[0].geometry.location;
         locationReturn.cityName = res.results[0].formatted_address;
         locationReturn.lat = res.results[0].geometry.location.lat;
@@ -30,28 +35,29 @@ runApp.weatherInfo = function (res1, res2) {
     $.ajax({
         url: `https://api.darksky.net/forecast/${runApp.apiKeyTwo}/${locationReturn.lat},${locationReturn.lng}`,
         method: "GET",
-        dataType: "JSONP"
+        dataType: "JSONP",
+        data: {
+            units: "si"
+        }
     }).then((res) => {
-        // console.log(res);
+        console.log(res);
         const weatherReturn = {
             icon: res.currently.icon,
             temperature: res.currently.apparentTemperature,
             summary: res.currently.summary,
             humidity: res.currently.humidity,
-            windspeed: res.currently.windSpeed
+            windspeed: res.currently.windSpeed, //meters per second
+            longIcon: res.hourly.data[0].icon,
+            longTemperature: res.hourly.data[0].apparentTemperature,
+            longSummary: res.hourly.data[0].summary,
+            longHumidty: res.hourly.data[0].humidity,
+            longWindspeed: res.hourly.data[0].windSpeed 
         };
         console.log(weatherReturn);
+
         //call function that prints weatherReturn to DOM
-        let weatherPrinter = function () {
-            $(".weather").empty();
-            $(".weather").append(`<canvas id= "${weatherReturn.icon}" width="80" height="80"></canvas>`);
-            $(".weather").append(`<p>${weatherReturn.temperature}</p>`);
-            $(".weather").append(`<p>${weatherReturn.summary}</p>`);
-            $(".weather").append(`<p>${weatherReturn.humidity}</p>`);
-            $(".weather").append(`<p>${weatherReturn.windspeed}</p>`);
-            runApp.skyConLoader();
-        };
-        weatherPrinter();
+        runApp.weatherPrinter(weatherReturn);
+        runApp.blurbCondition(weatherReturn.temperature);
     });
 };
 
@@ -64,6 +70,18 @@ runApp.listenForSubmit = function(){
         runApp.geocode(userlocation);
     });
 }
+
+// FUNCTION THAT APPENDS CURRENT WEATHER TO DOM
+runApp.weatherPrinter = function (weatherReturn) {
+
+    $(".weather").empty();
+    $(".weather").append(`<canvas id= "${weatherReturn.icon}" width="80" height="80"></canvas>`);
+    $(".weather").append(`<p>Feels Like: ${Math.floor(weatherReturn.temperature)} °C</p>`);
+    $(".weather").append(`<p>${weatherReturn.summary}</p>`);
+    $(".weather").append(`<p>Humidity: ${Math.floor(weatherReturn.humidity * 100)} %</p>`);
+    $(".weather").append(`<p>Wind Speed: ${(weatherReturn.windspeed * 3.6).toFixed(2)} km/h</p>`);
+    runApp.skyConLoader();
+};
 
 // CREATING SKYCONS FUNCTION
 runApp.skyConLoader = function(){
@@ -98,6 +116,36 @@ runApp.init = function(){
 $(function(){
     runApp.init();
 });
+
+runApp.blurbCondition = (temperature) => {
+    let finalBlurb;
+    console.log(finalBlurb);
+    if (temperature < 0) {
+        finalBlurb = (blurbArray[0][Math.floor(Math.random()
+            * blurbArray.length)]);
+    } else if (temperature < 12) {
+        finalBlurb = (blurbArray[1][Math.floor(Math.random()
+            * blurbArray.length)]);
+    } else {
+        finalBlurb = (blurbArray[2][Math.floor(Math.random()
+            * blurbArray.length)]);
+    }
+    console.log(finalBlurb);
+    // this where we would append said Var to DOM 
+    runApp.blurbPrinter(finalBlurb);
+};
+
+
+
+
+
+runApp.blurbPrinter = (blurb) => {
+    $(".blurb").empty();
+    $('.blurb').append(`${blurb}`);
+
+};
+
+
 
 
 
