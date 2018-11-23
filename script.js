583d45a4f7813821de93ca7eb485e7947d4086e7
lucamorellato <luca.morellato@gmail.com>
2018-11-22 19:51:19 -0500

    Wow done some work eh

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ runApp.apiKey = `AIzaSyBXaDLbvS9m_4euOEdcVxbnybx8D7202C4`;
 runApp.apiKeyTwo = `12db71466624332600c66c1d7e474f6d`;
 let userlocation;
 let locationReturn = {};
+
+//array of blurbs that will be printed to DOM depending on the temperature of userlocation
 const blurbArray =  [
     ["Comment One", "Comment Two", "Comment Three"],
     ["Comment Four", "Comment Five", "Comment 6ix"],
@@ -40,20 +42,24 @@ runApp.weatherInfo = function (res1, res2) {
             units: "si"
         }
     }).then((res) => {
-        console.log(res);
+
+        //creating the object that contains out GET request from darksky
         const weatherReturn = {
             icon: res.currently.icon,
             temperature: res.currently.apparentTemperature,
             summary: res.currently.summary,
             humidity: res.currently.humidity,
             windspeed: res.currently.windSpeed, //meters per second
-            longIcon: res.hourly.data[0].icon,
-            longTemperature: res.hourly.data[0].apparentTemperature,
-            longSummary: res.hourly.data[0].summary,
-            longHumidty: res.hourly.data[0].humidity,
-            longWindspeed: res.hourly.data[0].windSpeed 
+
+            //requesting the weather forecast
+            longIcon: res.hourly.data[2].icon,
+            longTemperature: res.hourly.data[2].apparentTemperature,
+            longSummary: res.hourly.data[2].summary,
+            longHumidity: res.hourly.data[2].humidity,
+            longWindspeed: res.hourly.data[2].windSpeed //meters per second
         };
-        console.log(weatherReturn);
+
+        
 
         //call function that prints weatherReturn to DOM
         runApp.weatherPrinter(weatherReturn);
@@ -63,6 +69,7 @@ runApp.weatherInfo = function (res1, res2) {
 
 // SUBMIT EVENT LISTENER FOR LOCATION
 runApp.listenForSubmit = function(){
+    //listen for submit of the form
     $("form").on("submit", function (e) {
         e.preventDefault();
         let userlocation = $("#location").val();
@@ -73,13 +80,23 @@ runApp.listenForSubmit = function(){
 
 // FUNCTION THAT APPENDS CURRENT WEATHER TO DOM
 runApp.weatherPrinter = function (weatherReturn) {
+    
 
+    //printing of current weather
     $(".weather").empty();
-    $(".weather").append(`<canvas id= "${weatherReturn.icon}" width="80" height="80"></canvas>`);
-    $(".weather").append(`<p>Feels Like: ${Math.floor(weatherReturn.temperature)} °C</p>`);
+    $(".longWeather").empty();
+    $(".weather").append(`<canvas id="${weatherReturn.icon}" width="80" height="80"></canvas>`);
+    $(".weather").append(`<p>Feels Like: ${Math.floor(weatherReturn.temperature)} °C</p>`); //---- used math to round down the temperature
     $(".weather").append(`<p>${weatherReturn.summary}</p>`);
     $(".weather").append(`<p>Humidity: ${Math.floor(weatherReturn.humidity * 100)} %</p>`);
-    $(".weather").append(`<p>Wind Speed: ${(weatherReturn.windspeed * 3.6).toFixed(2)} km/h</p>`);
+    $(".weather").append(`<p>Wind Speed: ${(weatherReturn.windspeed * 3.6).toFixed(2)} km/h</p>`); // ----- used math to convert m/s into km/h and move decimal point
+
+    //printing of the weather forecast
+    $(".longWeather").append(`<canvas id="${weatherReturn.longIcon} 2" width="80" height="80"></canvas>`);
+    $(".longWeather").append(`<p>Feels Like: ${Math.floor(weatherReturn.longTemperature)} °C</p>`); // ---- used math to round down the temperature
+    $(".longWeather").append(`<p>${weatherReturn.longSummary}</p>`);
+    $(".longWeather").append(`<p>Humidity: ${Math.floor(weatherReturn.longHumidity * 100)} %</p>`);
+    $(".longWeather").append(`<p>Wind Speed: ${(weatherReturn.longWindspeed * 3.6).toFixed(2)} km/h</p>`); // ----- used math to convert m/s into km/h and move decimal point
     runApp.skyConLoader();
 };
 
@@ -87,15 +104,25 @@ runApp.weatherPrinter = function (weatherReturn) {
 runApp.skyConLoader = function(){
     let icons = new Skycons({ "color": "black" });
     icons.set("clear-day", Skycons.CLEAR_DAY);
+    icons.set("clear-day 2", Skycons.CLEAR_DAY);
     icons.set("clear-night", Skycons.CLEAR_NIGHT);
+    icons.set("clear-night 2", Skycons.CLEAR_NIGHT);
     icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
+    icons.set("partly-cloudy-day 2", Skycons.PARTLY_CLOUDY_DAY);
     icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
+    icons.set("partly-cloudy-night 2", Skycons.PARTLY_CLOUDY_NIGHT);
     icons.set("cloudy", Skycons.CLOUDY);
+    icons.set("cloudy 2", Skycons.CLOUDY);
     icons.set("rain", Skycons.RAIN);
+    icons.set("rain 2", Skycons.RAIN);
     icons.set("sleet", Skycons.SLEET);
+    icons.set("sleet 2", Skycons.SLEET);
     icons.set("snow", Skycons.SNOW);
+    icons.set("snow 2", Skycons.SNOW);
     icons.set("wind", Skycons.WIND);
+    icons.set("wind 2", Skycons.WIND);
     icons.set("fog", Skycons.FOG);
+    icons.set("fog 2", Skycons.FOG);
     icons.play();
 };
 
@@ -144,6 +171,15 @@ runApp.blurbPrinter = (blurb) => {
     $('.blurb').append(`${blurb}`);
 
 };
+
+runApp.switchermadinger = () => {
+    $(".switch").on("click", function(){
+        $(".longWeather").toggleClass("open");
+        $(".weather").toggleClass("close");
+    });
+}
+
+runApp.switchermadinger();
 
 
 
